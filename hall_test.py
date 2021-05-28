9c5713ec433531f893983421cb20d040baa5dad0
Menyhert Fatyol <menyhert.fatyol@gmail.com>
2021-05-28 11:36:55 +0200

    Cleanup GPIO on keyboard interrupt

--- hall_test.py
+++ hall_test.py
@@ -1,9 +1,29 @@
-import RPi.GPIO as GPIO
+import time
+import importlib.util
+try:
+    importlib.util.find_spec('RPi.GPIO')
+    import RPi.GPIO as GPIO
+except ImportError:
+    """
+    import FakeRPi.GPIO as GPIO
+    OR
+    import FakeRPi.RPiO as RPiO
+    """
+
+    import FakeRPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(13, GPIO.IN)
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+input = GPIO.input(11)
 
-input = GPIO.input(13)
-while True:
-    if (input):
-        print("Locked")
+try:
+    while True:
+        if input:
+            print("No magnet")
+        else:
+            print("Magnet")
+
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
