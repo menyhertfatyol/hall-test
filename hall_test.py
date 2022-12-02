8714820cef84ed5a23066f298361bc51ad7da663
Menyhert Fatyol <menyhert.fatyol@gmail.com>
2022-12-02 11:36:41 +0100

    Update python version, replace fakerpi module with a working one

--- hall_test.py
+++ hall_test.py
@@ -3,15 +3,11 @@ import importlib.util
 try:
     importlib.util.find_spec('RPi.GPIO')
     import RPi.GPIO as GPIO
-except ImportError:
-    """
-    import FakeRPi.GPIO as GPIO
-    OR
-    import FakeRPi.RPiO as RPiO
-    """
+except (RuntimeError, ModuleNotFoundError):
+    import fake_rpigpio.utils
+    fake_rpigpio.utils.install()
 
-    import FakeRPi.GPIO as GPIO
-
+GPIO.VERBOSE = False
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
